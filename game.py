14776768e21f10e7d9d11854cc60639da726044d
Charles <charles.delafontaine@gmail.com>
2020-11-18 15:58:17 -0500

    completed

--- game.py
+++ game.py
@@ -4,51 +4,75 @@ Chapitre 11.1
 Classes pour représenter un personnage.
 """
 
-
 import random
 
 import utils
+UNARMED_POWER = 20
+
+
+def make_unarmed():
+	return Weapon("Unarmed", UNARMED_POWER, 1)
 
 
 class Weapon:
-	"""
-	Une arme dans le jeu.
-
-	:param name: Le nom de l'arme
-	:param power: Le niveau d'attaque
-	:param min_level: Le niveau minimal pour l'utiliser
-	"""
-
-	UNARMED_POWER = 20
+	def __init__(self, name, power, min_level):
+		self.name = name
+		self.power = power
+		self.min_level = min_level
 
 
 class Character:
-	"""
-	Un personnage dans le jeu
+	def __init__(self, name, hp, attack, defense, level, weapon=make_unarmed()):
+		self.name = name
+		self.hp = hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.weapon = weapon
 
-	:param name: Le nom du personnage
-	:param max_hp: HP maximum
-	:param attack: Le niveau d'attaque du personnage
-	:param defense: Le niveau de défense du personnage
-	:param level: Le niveau d'expérience du personnage
-	"""
+	def compute_damage(self, defender: "Character") -> tuple:
+		random_float = random.random()
+		if random_float < 0.0625:
+			crit = 2
+		else:
+			crit = 1
+		modifier = crit * random.randrange(85, 100) / 100
 
+		return (((2/5 * self.level + 2) * self.weapon.power * self.attack / defender.defense) / 50 + 2) * modifier, crit
 
+	def deal_damage(self, defender: "Character"):
+		damage = Character.compute_damage(self, defender)
+		defender.hp -= damage[0]
+		if defender.hp < 0:
+			defender.hp = 0
+		print(f"{self.name} used {self.weapon.name}")
+		if damage[1] == 2:
+			print("  Critical hit!")
+		print(f"{defender.name} took {round(damage[0],2)} DMG\n{defender.name} has now {round(defender.hp,2)} HP")
 
-def deal_damage(attacker, defender):
-	# TODO: Calculer dégâts
-	print(f"{attacker.name} used {attacker.weapon.name}")
-	if crit:
-		print("  Critical hit!")
-	print(f"  {defender.name} took {damage} dmg")
 
 def run_battle(c1, c2):
 	# TODO: Initialiser attaquant/défendeur, tour, etc.
+	tours = 0
+	attacker = c1
+	defender = c2
 	print(f"{attacker.name} starts a battle with {defender.name}!")
 	while True:
 		# TODO: Appliquer l'attaque
+		attacker.deal_damage(defender)
+
+
 		# TODO: Si le défendeur est mort
-			print(f"{defender.name } is sleeping with the fishes.")
+		if defender.hp == 0:
+			print(f"{defender.name} is sleeping with the fishes.")
 			break
-		# Échanger attaquant/défendeur
+		if tours % 2 == 0:
+			attacker = c2
+			defender = c1
+		else:
+			attacker = c1
+			defender = c2
+
+		tours += 1
+	return tours
 	# TODO: Retourner nombre de tours effectués
